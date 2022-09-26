75df76ce4f6e0a62e7770902783d181d2cbe9248
charliecondor <106284572+charliecondor@users.noreply.github.com>
2022-09-25 22:25:09 -0400

    Refactored addMenu selection logic to functions: addMealDate, addMealTime, addMealItem, removeMealItem

--- CaloricIntakeConsole/Program.cs
+++ CaloricIntakeConsole/Program.cs
@@ -19,7 +19,7 @@ namespace CaloricIntakeConsole
             while (userSelection != 0)
             {
                 Console.Title = "Caloric Intake Console App";
-                displayMainmenu(errorCode);
+                displayMainMenu(errorCode);
                 try
                 {
                     userSelection = Convert.ToInt32(Console.ReadLine());
@@ -39,8 +39,7 @@ namespace CaloricIntakeConsole
             }
             writeJSON(mealHistory, fileJSON);
         }
-
-        static void displayMainmenu(int error_code)
+        static void displayMainMenu(int error_code)
         {
             errorOutput(error_code);
             appTitle();
@@ -97,7 +96,7 @@ namespace CaloricIntakeConsole
             foreach (Meal meal in mealHistory.meals)
             {
                 if (!dailySummary.Exists(x => x.Date == meal.Date))
-                {                    
+                {
                     dailySummary.Add(new DailySummary { Date = meal.Date, TotalCalories = 0 });
                 }
             }
@@ -108,7 +107,7 @@ namespace CaloricIntakeConsole
                 foreach (Meal meal in mealHistory.meals)
                 {
                     if (day.Date == meal.Date)
-                    {                        
+                    {
                         foreach (MealItems mealitem in meal.mealitems)
                         {
                             day.TotalCalories += mealitem.Calories;
@@ -128,7 +127,7 @@ namespace CaloricIntakeConsole
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Write(item.TotalCalories);
                 Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("\t\t\t|");                
+                Console.WriteLine("\t\t\t|");
             }
             Console.WriteLine("+---------------+-----------------------+");
             Console.WriteLine("| Summary                               |");
@@ -139,21 +138,21 @@ namespace CaloricIntakeConsole
             int lifetime_low_calories = 100000;
             string lifetime_high_date = "";
             string lifetime_low_date = "";
-            foreach (DailySummary day in dailySummary) 
-            { 
+            foreach (DailySummary day in dailySummary)
+            {
                 lifetime_total_calories += day.TotalCalories;
-                if (lifetime_high_calories < day.TotalCalories) 
-                { 
+                if (lifetime_high_calories < day.TotalCalories)
+                {
                     lifetime_high_calories = day.TotalCalories;
                     lifetime_high_date = day.Date;
                 }
-                if (lifetime_low_calories > day.TotalCalories && day.Date != dailySummary.Last().Date) 
-                { 
+                if (lifetime_low_calories > day.TotalCalories && day.Date != dailySummary.Last().Date)
+                {
                     lifetime_low_calories = day.TotalCalories;
                     lifetime_low_date = day.Date;
                 }
             }
-            int lifetime_average_calories = lifetime_total_calories / dailySummary.Count;            
+            int lifetime_average_calories = lifetime_total_calories / dailySummary.Count;
             Console.Write("| Lifetime total: ");
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write(lifetime_total_calories);
@@ -184,13 +183,13 @@ namespace CaloricIntakeConsole
             while (last_ten < dailySummary.Count - 1)
             {
                 tenday_total_calories += dailySummary[last_ten].TotalCalories;
-                if (tenday_high_calories < dailySummary[last_ten].TotalCalories) 
+                if (tenday_high_calories < dailySummary[last_ten].TotalCalories)
                 {
                     tenday_high_calories = dailySummary[last_ten].TotalCalories;
                     tenday_high_date = dailySummary[last_ten].Date;
                 }
                 else if (tenday_low_calories > dailySummary[last_ten].TotalCalories)
-                {                    
+                {
                     tenday_low_calories = dailySummary[last_ten].TotalCalories;
                     tenday_low_date = dailySummary[last_ten].Date;
                 }
@@ -249,130 +248,23 @@ namespace CaloricIntakeConsole
 
                 if (userSelection == 1)
                 {
-                    Console.Write("Enter Meal Date [YYYY/MM/DD]: ");
-                    string meal_date = Console.ReadLine();
-                    char[] meal_date_char_codes = meal_date.ToCharArray();
-
-                    if (meal_date_char_codes.Length != 10) { error_code = 2; }
-                    else if (meal_date_char_codes[0] < 48 || meal_date_char_codes[0] > 57) { error_code = 2; }
-                    else if (meal_date_char_codes[1] < 48 || meal_date_char_codes[1] > 57) { error_code = 2; }
-                    else if (meal_date_char_codes[2] < 48 || meal_date_char_codes[2] > 57) { error_code = 2; }
-                    else if (meal_date_char_codes[3] < 48 || meal_date_char_codes[3] > 57) { error_code = 2; }
-                    else if (meal_date_char_codes[4] != 47) { error_code = 2; }
-                    else if (meal_date_char_codes[5] < 48 || meal_date_char_codes[5] > 57) { error_code = 2; }
-                    else if (meal_date_char_codes[6] < 48 || meal_date_char_codes[6] > 57) { error_code = 2; }
-                    else if (meal_date_char_codes[7] != 47) { error_code = 2; }
-                    else if (meal_date_char_codes[8] < 48 || meal_date_char_codes[8] > 57) { error_code = 2; }
-                    else if (meal_date_char_codes[9] < 48 || meal_date_char_codes[9] > 57) { error_code = 2; }
-                    else
-                    {
-                        error_code = 0;
-                        temp_meal.Date = meal_date;
-                    }
+                    temp_meal.Date = addMealDate(ref error_code);
                 }
                 else if (userSelection == 2)
                 {
-                    Console.Write("Enter Meal Time [HH:MM]: ");
-                    string meal_time = Console.ReadLine();
-                    char[] meal_time_char_codes = meal_time.ToCharArray();
-
-                    if (meal_time_char_codes.Length != 5) { error_code = 2; }
-                    else if (meal_time_char_codes[0] < 48 || meal_time_char_codes[0] > 50) { error_code = 2; }
-                    else if (meal_time_char_codes[1] < 48 || meal_time_char_codes[1] > 57) { error_code = 2; }
-                    else if (meal_time_char_codes[2] != 58) { error_code = 2; }
-                    else if (meal_time_char_codes[3] < 48 || meal_time_char_codes[3] > 53) { error_code = 2; }
-                    else if (meal_time_char_codes[4] < 48 || meal_time_char_codes[4] > 57) { error_code = 2; }
-                    else
-                    {
-                        error_code = 0;
-                        temp_meal.Time = meal_time;
-                    }
+                    temp_meal.Time = addMealTime(ref error_code);
                 }
                 else if (userSelection == 3)
                 {
-                    Console.WriteLine("Entering Meal Item (QTY/UNIT/DESC/CAL)...");
-                    Console.Write("Enter Quantity: ");
-                    string item_qty = Console.ReadLine();
-
-                    Console.Write("Enter Unit: ");
-                    string item_unit = Console.ReadLine();
-
-                    Console.Write("Enter Description: ");
-                    string item_desc = Console.ReadLine();
-
-                    Console.Write("Enter Calories: ");
-                    string str_item_cals = Console.ReadLine();
-                    int int_item_cals = new int();
-
-                    try
-                    {
-                        int_item_cals = Convert.ToInt32(str_item_cals);
-                        error_code = 0;
-                        temp_items.Add(new MealItems { Quantity = item_qty, UnitMeasurement = item_unit, Description = item_desc, Calories = int_item_cals });
-                        temp_meal.mealitems = temp_items;
-                    }
-                    catch
-                    {
-                        error_code = 1;
-                        userSelection = -1;
-                    }
+                    temp_meal.mealitems.Add(addMealItem(ref error_code));
                 }
                 else if (userSelection == 4)
                 {
-                    if (temp_meal.mealitems == null || temp_meal.mealitems.Count == 0)
-                    {
-                        error_code = 4;
-                    }
-                    else
-                    {
-                        Console.Write("Select Meal Item to Remove [Entry #]: ");
-                        string str_entry_to_remove = Console.ReadLine();
-                        int int_entry_to_remove = new int();
-
-                        try
-                        {
-                            int_entry_to_remove = Convert.ToInt32(str_entry_to_remove);
-
-                            if (int_entry_to_remove < 0 || int_entry_to_remove > temp_meal.mealitems.Count)
-                            {
-                                error_code = 1;
-                                userSelection = -1;
-                            }
-                            else
-                            {
-                                temp_meal.mealitems.RemoveAt(int_entry_to_remove);
-                                error_code = 0;
-                            }
-                        }
-                        catch
-                        {
-                            error_code = 1;
-                            userSelection = -1;
-                        }
-                    }
+                    removeMealItem(ref error_code, ref temp_meal);                                         
                 }
                 else if (userSelection == 0)
                 {
-                    if (temp_meal.Date == null)
-                    {
-                        error_code = 3;
-                        userSelection = -1;
-                    }
-                    else if (temp_meal.Time == null)
-                    {
-                        error_code = 3;
-                        userSelection = -1;
-                    }
-                    else if (temp_meal.mealitems == null)
-                    {
-                        error_code = 3;
-                        userSelection = -1;
-                    }
-                    else
-                    {
-                        error_code = 0;
-                        userSelection = 0;
-                    }
+                    checkMealItems(ref error_code, ref temp_meal, ref userSelection);
                 }
             }
             mealHistory.AddMeal(temp_meal);
@@ -505,12 +397,104 @@ namespace CaloricIntakeConsole
             string mealJSON = JsonSerializer.Serialize(mealHistory, options);
             File.WriteAllText(fileJSON, mealJSON);
         }
+        static string addMealDate(ref int error_code)
+        {
+            Console.Write("Enter Meal Date [YYYY/MM/DD]: ");
+            string meal_date = Console.ReadLine();
+            try
+            {                
+                DateTime mealDate = new DateTime(Convert.ToInt32(meal_date.Substring(0, 4)),
+                    Convert.ToInt32(meal_date.Substring(5, 2)),
+                    Convert.ToInt32(meal_date.Substring(8, 2)));
+                error_code = 0;
+                return mealDate.ToString("yyyy/MM/dd");
+            }
+            catch
+            {
+                error_code = 2;
+                return "";
+            }
+        }
+        static string addMealTime(ref int error_code)
+        {
+            Console.Write("Enter Meal Time [HH:MM]: ");
+            string meal_time = Console.ReadLine();
+            try
+            {
+                DateTime mealTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day,
+                    Convert.ToInt32(meal_time.Substring(0, 2)), Convert.ToInt32(meal_time.Substring(3, 2)), 0);
+                error_code = 0;
+                return mealTime.ToString("HH:mm");
+            }
+            catch
+            {
+                error_code = 2;
+                return "";
+            }
+        }
+        static MealItems addMealItem(ref int error_code)
+        {
+            Console.WriteLine("Entering Meal Item (QTY/UNIT/DESC/CAL)...");
+            Console.Write("Enter Quantity: ");
+            string item_qty = Console.ReadLine();
+            Console.Write("Enter Unit: ");
+            string item_unit = Console.ReadLine();
+            Console.Write("Enter Description: ");
+            string item_desc = Console.ReadLine();
+            Console.Write("Enter Calories: ");
+            string str_item_cals = Console.ReadLine();
+
+            try
+            {                
+                int int_item_cals = Convert.ToInt32(str_item_cals);
+                error_code = 0;
+                return new MealItems { Quantity = item_qty, UnitMeasurement = item_unit, 
+                    Description = item_desc, Calories = int_item_cals };
+            }
+            catch
+            {
+                error_code = 1;
+                return new MealItems();
+            }
+        }
+        static void removeMealItem(ref int error_code, ref Meal tempmeal)
+        {
+            if (tempmeal.mealitems.Count == 0) { error_code = 4; return; }
+            Console.Write("Select Meal Item to Remove [Entry #]: ");
+            string str_entry_to_remove = Console.ReadLine();
+
+            try
+            {
+                int int_entry_to_remove = Convert.ToInt32(str_entry_to_remove);
+                if (int_entry_to_remove > 0 || int_entry_to_remove < tempmeal.mealitems.Count)
+                {
+                    tempmeal.mealitems.RemoveAt(int_entry_to_remove);
+                    error_code = 0;
+                }
+            }
+            catch
+            {
+                error_code = 1;
+            }
+        }
+        static void checkMealItems(ref int error_code, ref Meal tempmeal, ref int userSelection)
+        {
+            if (tempmeal.Date == null || tempmeal.Time == null || tempmeal.mealitems == null)
+            {
+                error_code = 3;
+                userSelection = -1;
+            }
+            else
+            {
+                error_code = 0;
+                userSelection = 0;
+            }
+        }
     }
 
     public class MealHistory
     {
         public List<Meal> meals = new List<Meal>();
-
         public bool AddMeal(Meal meal)
         {
             meals.Add(new Meal { Date = meal.Date, Time = meal.Time, mealitems = meal.mealitems });
